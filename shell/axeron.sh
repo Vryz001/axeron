16e8a2ce5c3d56e9e46ada847faaab898f5bb230
Vryz001 <141978805+Vryz001@users.noreply.github.com>
2024-03-21 19:58:24 +0700

    Update axeron.sh

--- shell/axeron.sh
+++ shell/axeron.sh
@@ -2,4 +2,6 @@ export AXERON=true
 export CORE="d8a97692ad1e71b1"
 export EXECPATH=$(dirname $0)
 export PACKAGES=$(cat ${EXECPATH}/packages.list)
+this_core=$(dumpsys package "com.fhrz.axeron" | grep "signatures" | cut -d '[' -f 2 | cut -d ']' -f 1)
+if ! echo "$CORE" | grep -q "$this_core"; then; echo "$w You must use the original version of Axeron"; exit 0; fi
 !myCommands
