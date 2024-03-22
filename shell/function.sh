84912d4b842c0fa0d5426f8c72b323065ee280b4
Vryz001 <141978805+Vryz001@users.noreply.github.com>
2024-03-22 14:32:00 +0700

    Update function.sh

--- shell/function.sh
+++ shell/function.sh
@@ -1,1 +1,12 @@
+AXERON=true;
+CORE="d8a97692ad1e71b1";
+EXECPATH=$(dirname $0); 
+PACKAGES=$(cat ${EXECPATH}/packages.list)
+this_core=$(dumpsys package "com.fhrz.axeron" | grep "signatures" | cut -d '[' -f 2 | cut -d ']' -f 1)
 
+check_axeron() {
+  if ! echo "$CORE" | grep -q "$this_core"; then
+    echo "$w You must use the original version of Axeron"
+    exit 0
+  fi
+}
