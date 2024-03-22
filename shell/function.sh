ce859d81fb8252b3ed98b6bcc0ae4ff8d021bcb5
Vryz001 <141978805+Vryz001@users.noreply.github.com>
2024-03-22 19:13:10 +0700

    Update function.sh

--- shell/function.sh
+++ shell/function.sh
@@ -1,7 +1,7 @@
-export AXERON=true
-export CORE="d8a97692ad1e71b1"
-export EXECPATH=$(dirname $0)
-export PACKAGES=$(cat ${EXECPATH}/packages.list)
+AXERON=true
+CORE="d8a97692ad1e71b1"
+EXECPATH=$(dirname $0)
+PACKAGES=$(cat ${EXECPATH}/packages.list)
 this_core=$(dumpsys package "com.fhrz.axeron" | grep "signatures" | cut -d '[' -f 2 | cut -d ']' -f 1)
 
 check_axeron() {
