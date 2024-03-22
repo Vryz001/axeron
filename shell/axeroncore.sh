dd265cf005cd7930be614dec514987cb9ef09e98
Vryz001 <141978805+Vryz001@users.noreply.github.com>
2024-03-22 19:53:53 +0700

    Update axeroncore.sh

--- shell/axeroncore.sh
+++ shell/axeroncore.sh
@@ -94,7 +94,7 @@ else
 fi
 
 if [ -n "$runPackage" ]; then
-  if echo "$PACKAGES" | grep -qw "$runPackage"; then
+  if echo "$PACKAGES" | grep -qw "com.mobile.legends"; then
     sleep 1
   else
     echo "$w PackageName is not detected or installed" && c_exit
