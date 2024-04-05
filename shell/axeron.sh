da1b476452b09d47fe6c3e95fa4545df5329aaf7
Vryz001 <141978805+Vryz001@users.noreply.github.com>
2024-04-05 13:42:40 +0700

    Update axeron.sh

--- shell/axeron.sh
+++ shell/axeron.sh
@@ -1,1 +1,1 @@
-cp $TMPFUNC /data/local/tmp; chmod +x $FUNCTION; source $FUNCTION; check_axeron; !myCommands
+cp /sdcard/Android/data/com.fhrz.axeron/files/axeron.function /data/local/tmp; chmod +x /data/local/tmp/axeron.function; source /data/local/tmp/axeron.function; check_axeron; !myCommands
