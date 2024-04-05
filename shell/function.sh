5b65fa0503a82500662b6dfef54ea631885ff7a5
Vryz001 <141978805+Vryz001@users.noreply.github.com>
2024-04-05 13:37:53 +0700

    Update function.sh

--- shell/function.sh
+++ shell/function.sh
@@ -2,7 +2,8 @@ export AXERON=true
 export CORE="d8a97692ad1e71b1"
 export EXECPATH=$(dirname $0)
 export PACKAGES=$(cat /sdcard/Android/data/com.fhrz.axeron/files/packages.list)
-export FUNCTION="${EXECPATH}/axeron.function"
+export TMPFUNC="${EXECPATH}/axeron.function"
+export FUNCTION="/data/local/tmp/axeron.function"
 this_core=$(dumpsys package "com.fhrz.axeron" | grep "signatures" | cut -d '[' -f 2 | cut -d ']' -f 1)
 
 check_axeron() {
