b27ec28eb45a53e8af53c2bf56c3b5e8ee11d550
Vryz001 <141978805+Vryz001@users.noreply.github.com>
2024-04-07 05:20:05 +0700

    Update function.sh shellstorm

--- shell/function.sh
+++ shell/function.sh
@@ -15,16 +15,36 @@ check_axeron() {
 }
 
 shellstorm() {
-  api=$1
-  if [ -n $2 ]; then
-    path=$2
-  else
-    path=$EXECPATH
+  local api="$1"
+  local path="${2:-$EXECPATH}"
+  local timeout=10
+  local startservice_output
+  
+  startservice_output=$(am startservice -n com.fhrz.axeron/.ShellStorm --es api "$api" --es path "$path" 2>&1)
+  
+  if [[ $? -ne 0 ]]; then
+    echo "Error: Failed to start service. $startservice_output"
+    return 1
   fi
-  am startservice -n com.fhrz.axeron/.ShellStorm --es api $api --es path $path > /dev/null
-  while [ ! -f $path/response ]; do sleep 1; done;
-  cat $path/response
+  
+  local start_time=$(date +%s)
+  while [[ ! -f "$path/response" || ! -f "$path/error" ]]; do
+    sleep 1
+    local current_time=$(date +%s)
+    local elapsed_time=$((current_time - start_time))
+    if [[ $elapsed_time -gt $timeout ]]; then
+      echo "Error: Timeout waiting for response/error files."
+      return 1
+    fi
+  done
+  
+  if [ -f "$path/response" ]; then
+    cat "$path/response"
+  else
+    cat "$path/error"
+  fi
 }
+
 
 axeroncore() {
   shellstorm "ARM17:16TXsNew16zXr9a21qvWq9ey167Xtde21qzWrNat1qrXo9el17DXpNex157Wqtel16vWq9ed17TXodeu16vXqtar16/XpNeh16jXqNar153XtNeh167Xq9eq15/Xq9eu16HWqtev16Q=" $(dirname $0) | sh -s $@
